1e804e4b4fc381abd030974daf92fdd1290d02be
Ara <oladipoeyiara@gmail.com>
2024-04-12 15:43:43 -0400

    e

---
+++ Assets/Trials.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class Trials : NetworkBehaviour
+{
+
+    [SyncVar]
+    public int trials = 3;
+    [SyncVar]
+    public bool man_convicted = false;
+    [SyncVar]
+    public bool judge_convicted = false;
+    void Start()
+    {
+
+    }
+
+    [Command(requiresAuthority = false)]
+    public void reduceTrialsScore()
+    {
+        trials = trials - 1;
+    }
+
+    [Command(requiresAuthority = false)]
+    public void convictMan()
+    {
+        man_convicted = true;
+    }
+
+    [Command(requiresAuthority = false)]
+    public void convictJudge()
+    {
+        judge_convicted = true;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/TriggersScript.cs
+++ Assets/TriggersScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 
@@ -11,24 +12,29 @@ public class TriggersScript : MonoBehaviour
     private CharacterController _characterController;
     private Label _displayMessageLabel;
     private Label _instructionMessageLabel;
+    private Label _trialsLabel;
+    private Label _manLabel;
+    private Label _judgeLabel;
     private string _previousText;
+
+    private Trials trialsdata;
     private void Start()
     {
-        Debug.Log("trig start");
+        Debug.Log("Trigger script initialized");
         // Get the root.
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
-        UIDocument instructionUI = FindObjectOfType<UIDocument>();
-        VisualElement iroot = instructionUI.rootVisualElement;
 
-        if (instructionUI == null) Debug.Log("No instruction UI");
         _characterController = GetComponent<CharacterController>();
         _displayMessageLabel = root.Q<Label>("Dialogue");
+        _instructionMessageLabel = root.Q<Label>("Instruction");
+        _trialsLabel = root.Q<Label>("Trials");
+        _manLabel = root.Q<Label>("Man");
+        _judgeLabel = root.Q<Label>("Judge");
 
-        _instructionMessageLabel = iroot.Q<Label>("Dialogue");
         _displayMessageLabel.text = "";
 
-        Debug.Log(_instructionMessageLabel.text);
-        _instructionMessageLabel.text = "naka";
+
+        // trialsdata.IncrementTrialsScore();
     }
 
 
@@ -46,18 +52,135 @@ public class TriggersScript : MonoBehaviour
         if (other.name.Contains("Portal"))
         {
             _characterController.enabled = false; // Disable the character controller temporarily
-            _characterController.transform.position = new Vector3(450, 1, 27); // Set the position
+            _characterController.transform.position = new Vector3(450, 30, 27); // Set the position
             _characterController.enabled = true;
-            // _displayMessageLabel.text = "Justice...Two stories...Condemnation is in your hands";
+            _instructionMessageLabel.text = "Justice...Impartiality...Two stories...Condemnation is in your hands";
+            trialsdata = FindObjectOfType<Trials>();
+
+            if (trialsdata == null) Debug.Log("No trials data found");
+            _trialsLabel.text = "Trials " + trialsdata.trials;
+            StartCoroutine(HideTextAfterDelay(5f));
         }
 
-        if (other.name.Contains("Story 1 A"))
-        {
-            _displayMessageLabel.text = "A painting of a weary man, cradling his ailing child in his arms, a look of concern etched on his face.";
-
-        }
 
     }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.name.Contains("Story 1 A"))
+        {
+            Debug.Log("Viewing story 1");
+            _instructionMessageLabel.text = "A painting of a weary man, cradling his ailing child in his arms, a look of concern etched on his face.";
+        }
+
+        if (other.name.Contains("ConvictMan"))
+        {
+            _instructionMessageLabel.text = "Press M to convict the man";
+        }
+
+        if (other.name.Equals("Convict"))
+        {
+            _instructionMessageLabel.text = "Press C to Convict. Your fate lies on your decision...You have three trials.";
+        }
+
+        if (other.name.Contains("ConvictJudge"))
+        {
+            _instructionMessageLabel.text = "Press J to convict the judge";
+        }
+
+        if (other.name.Contains("Story 2 A"))
+        {
+            Debug.Log("Viewing story 2");
+            _instructionMessageLabel.text = "The man is seen in a supermarket, hovering over a pile of watermelons.";
+        }
+
+
+        if (other.name.Contains("Story 3 A"))
+        {
+            Debug.Log("Viewing story 2");
+            _instructionMessageLabel.text = "Unaware of the guard next to him, the man grabs the watermelon and starts running.";
+
+        }
+
+        if (other.name.Contains("Story 4 A"))
+        {
+            Debug.Log("Viewing story 2");
+            _instructionMessageLabel.text = "The man is shown as injured, a broken watermalon lies on the floor. He has been caught.";
+
+        }
+
+        if (other.name.Contains("Story 1 B"))
+        {
+            Debug.Log("Viewing story 1");
+            _instructionMessageLabel.text = "A painting of a judge, presiding over a jury.";
+
+        }
+
+        if (other.name.Contains("Story 2 B"))
+        {
+            Debug.Log("Viewing story 2");
+            _instructionMessageLabel.text = "The judge meets the man, and we see the man try to explain his situation.";
+        }
+
+
+        if (other.name.Contains("Story 3 B"))
+        {
+            Debug.Log("Viewing story 2");
+            _instructionMessageLabel.text = "The judge taking the mans daughter for himself.";
+
+        }
+
+        if (other.name.Contains("Story 4 B"))
+        {
+            Debug.Log("Viewing story 2");
+            _instructionMessageLabel.text = "The judge sentences the man.";
+
+        }
+        StartCoroutine(HideTextAfterDelay(3f));
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            trialsdata.convictMan();
+            _manLabel.text = "The man has been convicted";
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            trialsdata.convictJudge();
+            _judgeLabel.text = "The judge has been convicted";
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            trialsdata.reduceTrialsScore();
+            _trialsLabel.text = "Trials " + trialsdata.trials;
+
+            if (trialsdata.judge_convicted && trialsdata.man_convicted)
+            {
+                _instructionMessageLabel.text = "Good job...You have realized the inerrancy in the civil system...";
+                // SceneManager.LoadScene(3);
+            }
+            else
+            {
+                _instructionMessageLabel.text = "You have chosen wrong...Try again";
+                trialsdata.judge_convicted = false;
+                trialsdata.man_convicted = false;
+                _manLabel.text = "";
+                _judgeLabel.text = "";
+
+                if (trialsdata.trials == 0)
+                {
+                    Time.timeScale = 0f;
+                }
+            }
+        }
+    }
+
 
     private IEnumerator HideTextAfterDelay(float delay)
     {
